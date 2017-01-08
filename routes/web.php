f38f677403799d0a075d270d052dfb3b1cad625e
billion102 <billion102@outlook.com>
2017-01-09 00:20:16 +0800

    add authentication

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
 });
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
